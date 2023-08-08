9221e623f7709030a2c5e0e7f3ad7d479b441ad4
gnanendramittaie <141755109+gnanendramittaie@users.noreply.github.com>
2023-08-08 21:22:17 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,10 @@
+//Sum using Methods
+class Sum{
+public static void main(String []arg){
+Sum Sobj = new Sum();
+Sobj.Summo(5,6);
+}
+void Summo(int a,int b){
+System.out.println("Sum is : "+(a+b));
+}
+}
